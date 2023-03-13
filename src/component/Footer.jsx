a2061dba7681b168e2056061bf1647c5a74b2a1d
MALWICK <batahumphrey66@gmail.com>
2023-03-13 12:38:11 +0100

    creating my first footer container

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,19 +5,19 @@ function Footer() {
   return (
     <div className={Style.container}>
       <div className={Style.cont1}>
-        <div>
+        <div className={Style.cont_small1}>
           <i className="fa-solid fa-box" />
           <p>Same-day local delivery</p>
         </div>
-        <div>
+        <div className={Style.cont_small2}>
           <i className="fa-solid fa-truck-fast" />
           <p>Ground shipping directly to you</p>
         </div>
-        <div>
+        <div className={Style.cont_small3}>
           <i className="fa-solid fa-house-chimney" />
           <p>Free in-store pickup</p>
         </div>
-        <div>
+        <div className={Style.cont_small4}>
           <i className="fa-solid fa-headset" />
           <p>24/7 customer support</p>
         </div>
