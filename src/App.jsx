37face99fbd6e447ab0c42a42a60830f51761808
MALWICK <batahumphrey66@gmail.com>
2023-03-13 10:52:00 +0100

    creating login and register Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
+import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route index element={<Login />} />
+          <Route index element={<Home />} />
           <Route path="/register" element={<Register />} />
+
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
