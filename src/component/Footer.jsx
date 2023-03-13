099808550aa4e8fbed25e7b43bdcd6a59af833d3
MALWICK <batahumphrey66@gmail.com>
2023-03-13 13:02:19 +0100

    creating my first footer container

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -22,6 +22,51 @@ function Footer() {
           <p>24/7 customer support</p>
         </div>
       </div>
+      <div className={Style.cont2}>
+        <div>
+          <h4>CUSTOMER SERVICE</h4>
+          <ul>
+            <li>Help Center</li>
+            <li>My Account</li>
+            <li>Track My Order</li>
+            <li>Return Policy</li>
+            <li>Gifts Cards</li>
+          </ul>
+        </div>
+        <div>
+          <h4>ABOUT US</h4>
+          <ul>
+            <li>Company Info</li>
+            <li>Press Releases</li>
+            <li>Careers</li>
+            <li>Reviews</li>
+            <li>Investor Relation</li>
+          </ul>
+        </div>
+        <div>
+          <h4>QUICK LINKS</h4>
+          <ul>
+            <li>Search</li>
+            <li>Become a Resseller</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Terms of Service</li>
+          </ul>
+        </div>
+        <div>
+          <h4>CATEGORIES</h4>
+          <ul>
+            <li>Brandy</li>
+            <li>White Wine</li>
+            <li>Champagne</li>
+            <li>Vodka</li>
+            <li>Tequila</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Get exclusive offers & rewards</h3>
+        </div>
+      </div>
     </div>
   );
 }
